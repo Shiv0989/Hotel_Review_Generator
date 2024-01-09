d96a779348d0ee1451b3ab9736cf7fdd7baabb10
Shiv0989 <83575973+Shiv0989@users.noreply.github.com>
2024-01-10 00:56:24 +1100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from api_client import APIClient
 from streamlit_ui import StreamlitUI
 
 def main():
-    data_file_path = '/Users/shivatmaksharma/Downloads/AI data 1 1.xlsx'
+    data_file_path = 'Your_File_Path'
     data_manager = DataManager(data_file_path)
     api_client = APIClient()
     ui = StreamlitUI(data_manager, api_client)
